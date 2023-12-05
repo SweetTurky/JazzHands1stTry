78c590f0823a9e0ae3d228d5481aadc8dd3db5c7
Ousager3 <pan21@student.aau.dk>
2023-12-05 15:47:23 +0100

    Masser af kode min ven

--- Assets/Interrogation Room/Scripts/BiometricInputManager.cs
+++ Assets/Interrogation Room/Scripts/BiometricInputManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class BiometricInputManager : MonoBehaviour
 {
-    public bool isAngry = true;
+    public bool isAngry = false;
+    public int lieCounter;
+    public UDPReceive uDPReceive;
+    public SoundManager soundManager;
+    public EventManager eventManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +18,33 @@ public class BiometricInputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void IsAngry()
-    {
 
+    }
+    public void LieDetection()
+    {
+        StartCoroutine(LieDetector());
+    }
+    public IEnumerator LieDetector()
+    {
+        yield return new WaitForSeconds(3f);
+        lieCounter = 0;
+        
+        if (uDPReceive.gooseBumps == true)
+        {
+            lieCounter++;
+        }
+        if (heartRateMonitorScript.heartRateHigh == true)
+        {
+            lieCounter++;
+        }
+        if (galvanicSkinResponseScript.moistureHigh == true)
+        {
+            lieCounter++;
+        }
+        if (lieCounter >= 2)
+        {
+            isAngry = true;
+        }
+        eventManager.VoiceInput();
     }
 }
